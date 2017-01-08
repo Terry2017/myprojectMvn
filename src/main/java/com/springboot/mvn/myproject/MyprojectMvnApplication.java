2c2f19dcf22f679b2c1eebbf7de4dc1475f6d1e4
Terry2017 <terry2015zhb@163.com>
2017-01-08 15:49:31 +0800

    my project

---
+++ src/main/java/com/springboot/mvn/myproject/MyprojectMvnApplication.java
@@ -0,0 +1,12 @@
+package com.springboot.mvn.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyprojectMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyprojectMvnApplication.class, args);
+	}
+}
